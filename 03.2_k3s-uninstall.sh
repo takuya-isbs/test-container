acd5bd6c458d43fa2081a981b2e46fb5bac85ea4
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2023-05-22 23:03:32 +0900

    03.2_k3s-uninstall.sh: ignore error

--- 03.2_k3s-uninstall.sh
+++ 03.2_k3s-uninstall.sh
@@ -7,7 +7,7 @@ NODE_1=${NODE_PREFIX}1
 
 for i in `seq 2 $NUM_NODES`; do
     NODE_NAME=${NODE_PREFIX}${i}
-    lxc exec ${NODE_NAME} -- k3s-agent-uninstall.sh
+    lxc exec ${NODE_NAME} -- k3s-agent-uninstall.sh || true
 done
 
 lxc exec ${NODE_1} -- k3s-uninstall.sh
